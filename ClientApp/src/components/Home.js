7a1ce3fd0987317e8a3b7c61be175316b218d178
Leonardo Garcia <lgarcia@webstaurantstore.com>
2019-02-06 15:49:00 -0500

    Clean up home page and add icons

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,22 +7,27 @@ export class Home extends Component {
 
   render() {
     return (
-      <div className="card-container">
-        <NewsHeaderCard
-          title="Add order"
-          href="https://github.com/nekonee"
-          thumbnail={require("../Images/addCircle.svg")}
-        />
-        <NewsHeaderCard
-          title="View order history"
-          href="https://github.com/nekonee"
-          thumbnail={require("../Images/La.Panaderia.jpg")}
-        />
-        <NewsHeaderCard
-          title="Recipes"
-          href="https://github.com/nekonee"
-          thumbnail={require("../Images/La.Panaderia.jpg")}
-        />
+      <div>
+        <div className="card-container">
+          <NewsHeaderCard
+            title="Add order"
+            href="./Counter"
+            thumbnail={require("../Images/addCircle.svg")}
+          />
+          <NewsHeaderCard
+            title="View order history"
+            href="https://github.com/nekonee"
+            thumbnail={require("../Images/La.Panaderia.jpg")}
+          />
+        </div>
+
+        <div className="card-container">
+          <NewsHeaderCard
+            title="Recipes"
+            href="https://github.com/nekonee"
+            thumbnail={require("../Images/La.Panaderia.jpg")}
+          />
+        </div>
       </div>
     );
   }
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
-import { NavMenu } from './NavMenu';
+import React, { Component } from "react";
+import { Col, Grid, Row } from "react-bootstrap";
+import { NavMenu } from "./NavMenu";
 
 export class Layout extends Component {
-  displayName = Layout.name
+  displayName = Layout.name;
 
   render() {
     return (
-      <Grid fluid>
+      <Grid>
         <Row>
           <Col sm={3}>
             <NavMenu />
           </Col>
-          <Col sm={9}>
-            {this.props.children}
-          </Col>
+
+          <Col sm={9}>{this.props.children}</Col>
         </Row>
       </Grid>
     );
